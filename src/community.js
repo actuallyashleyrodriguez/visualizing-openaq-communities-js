e22c99219d35480626d399491fceab686179e005
Arodriguez1030 <arodriguez1030@gmail.com>
2021-06-18 18:54:52 -0400

    created table

--- src/community.js
+++ src/community.js
@@ -35,6 +35,100 @@ class Community {
         
         document.querySelector(".community-container").appendChild(communityDiv);
     }
+    filterCommunity() {
+        this.average >=5 ?  communitiesMeetingCriteria.push(this) : communitiesNotMeetingCriteria.push(this)
+        
+    }
+    filterCriteriaCountry() {
+        let update = {
+            name: this.name,
+            value: this.average
+        }
+        if(this.average >=5)
+        switch (this.country) {
+            case 'US':
+                criteriaDataPerCountry[0].data.push(update);
+              break;
+            case 'CA':
+                criteriaDataPerCountry[1].data.push(update);
+            break;
+            case 'GB':
+                criteriaDataPerCountry[2].data.push(update);
+            break;
+            case 'RS':
+                criteriaDataPerCountry[3].data.push(update);
+            break;
+            case 'GR':
+                criteriaDataPerCountry[4].data.push(update);
+            break;
+            case 'NO':
+                criteriaDataPerCountry[5].data.push(update);
+            break;
+            case 'KR':
+                criteriaDataPerCountry[6].data.push(update);
+            break;
+            case 'AU':
+                criteriaDataPerCountry[7].data.push(update);
+            break;
+            case 'BA':
+                criteriaDataPerCountry[8].data.push(update);
+            break;
+            case 'IN':
+                criteriaDataPerCountry[9].data.push(update);
+            break;
+            case 'BR':
+                criteriaDataPerCountry[10].data.push(update);
+            break;
+            case 'MX':
+                criteriaDataPerCountry[11].data.push(update);
+            break;
+          }
+    }
+    
 }
 
 Community.all = []
+let communitiesMeetingCriteria = []
+let communitiesNotMeetingCriteria = []
+let criteriaDataPerCountry = [{
+    name: 'US',
+    data:  []
+}, {
+    name: 'Canada',
+    data: []
+}, {
+    name: 'United Kingdom',
+    data: []
+}, {
+    name: 'Serbia',
+    data: []
+}, {
+    name: 'Greece',
+    data: []
+}, {
+    name: 'Norway',
+    data: []
+}, {
+    name: 'South Korea',
+    data: []
+}, {
+    name: 'Australia',
+    data: []
+}, {
+    name: 'Bosnia',
+    data: []
+},
+{
+    name: 'India',
+    data: []
+},
+{
+    name: 'Brazil',
+    data: []
+},
+{
+    name: 'Mexico',
+    data: []
+}]
+
+
--- src/index.js
+++ src/index.js
@@ -1,70 +1,118 @@
 const openAQRoute = "https://cors-anywhere.herokuapp.com/https://docs.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&unit=%C2%B5g%2Fm%C2%B3&radius=1000&order_by=lastUpdated&dumpRaw=false"
 
 document.addEventListener("DOMContentLoaded", () => {
-    showCommunity()
+    showCommunity(), criteriaChart()
 });
 
 let showCommunity = () => {
     fetch(openAQRoute)
     .then(response => response.json())
     .then(commInfo => commInfo.results.forEach(commAttr =>  {
+        if (commAttr.hasOwnProperty('coordinates')) {
         let thisCommunity = new Community(commAttr, commAttr.coordinates, commAttr.parameters)
-      thisCommunity.renderCommunity()
-       
+      thisCommunity.renderCommunity(), thisCommunity.filterCommunity(), thisCommunity.filterCriteriaCountry()
+        }
    })
     )
 }
 
-var chart = JSC.chart('chartDiv', { 
-    debug: true, 
-    type: 'map', 
-    legend_visible: false, 
-    
-    title: { 
-      position: 'full', 
-      label_text: 'World Regions Projections'
-    }, 
-    
-    mapping_projection: false, 
-    
-    series: [ 
-      { 
-        /* 
-         * The world must be specified in order to load the correct map. This however only needs to be specified once. 
-         * Alternatively, setting mappingReferenceLayer to world also works. 
-         * */
-        map: 'world.region:africa'
-      }, 
-      { map: 'region:europe' }, 
-      { map: 'region:asia' }, 
-      { map: 'region:americas' } 
-    ], 
-    toolbar: { 
-      items: { 
-        resetZoom_position: 'inside bottom left', 
-        Projection: { 
-          type: 'select', 
-          value: 'none', 
-          items: { 
-            none: {}, 
-            mercator: {}, 
-            lambertConformalConic: {} 
-          }, 
-          events_change: projectionChanged 
-        } 
-      } 
-    } 
-  }); 
-    
-  function projectionChanged(val) { 
-    chart.options({ 
-      mapping_projection: { 
-        type: val, 
-        parallels: [11.5, 0] 
-      } 
-    }); 
-  } 
-    
-  function setProjection(val) { 
-    chart.options({ mapping_projection_type: val }); 
-  } 
+let criteriaChart = () => Highcharts.chart('container', {
+    chart: {
+        type: 'packedbubble',
+        height: '100%'
+    },
+    title: {
+        text: 'Air Pollution in communities with >=5 µg/m³ (based on PM2.5)'
+    },
+    tooltip: {
+        useHTML: true,
+        pointFormat: '<b>{point.name}:</b> {point.value} PM2.5>'
+    },
+    plotOptions: {
+        packedbubble: {
+            minSize: '20%',
+            maxSize: '100%',
+            zMin: 0,
+            zMax: 1000,
+            layoutAlgorithm: {
+                gravitationalConstant: 0.05,
+                splitSeries: true,
+                seriesInteraction: false,
+                dragBetweenSeries: true,
+                parentNodeLimit: true
+            },
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}',
+                filter: {
+                    property: 'y',
+                    operator: '>',
+                    value: 250
+                },
+                style: {
+                    color: 'black',
+                    textOutline: 'none',
+                    fontWeight: 'normal'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'US',
+        data:  [{
+            name: "KIT5153", 
+            value: 10.4846820174291
+        }, {
+            name: "Waterloo Pri", 
+            value: 12.4990254849519
+        }, {
+            name: "Pointe Woodworth", 
+            value: 6.87268375891657
+        }, {
+            name: "Rinda Drive", 
+            value: 5.45188255962387
+        }, {
+            name: "Rehoboth", 
+            value: 9.65888581952117
+        }, {
+            name: "USCEHC Lincoln Heights", 
+            value: 56.2716622628626
+        }, {
+            name: "Bel Air", 
+            value: 8.59996021608945
+        }, {
+            name: "Whitney Ranch", 
+            value: 12.552242919597
+        }, {
+            name: "Bonny Slope", 
+            value: 5.77926198201158
+        }, {
+            name: "MERCED", 
+            value: 6.21467508635579
+        }
+    ]
+    },  {
+        name: 'Canada',
+        data: [{
+            name: "NAPS-SNPA_AvinL5-1", 
+            value: 7.41430168743909
+        },{
+            name: "ACA_BelgraviaCL", 
+            value: 8.81092249480763
+        }, {
+            name: "cleanairplan.ca-RailwaySouth", 
+            value: 8.53025490568727
+        }]
+    }, {
+        name: 'United Kingdom',
+        data: [criteriaDataPerCountry[4].data]
+    },{
+        name: 'Bosnia',
+        data: [criteriaDataPerCountry[10].data]
+    },
+    ]
+});
+
+
+
+
